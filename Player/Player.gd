e7d0744e0102b7ca218c732970b851fa0f4a9ef7
Marc Sevilleja <marc@sevilayha.com>
2020-07-08 18:38:50 -0700

    Smoothed out jumping with an isJumping flag

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,32 +1,35 @@
 extends KinematicBody2D
 
-var motion = Vector2(0, 0)
-
 const SPEED = 500
-const GRAVITY = 9.81
-const UP = Vector2(0, -1)
-const JUMP_SPEED = 500
+const GRAVITY = 50
+const UP = Vector2.UP		# Vector2(0, -1)
+const JUMP_SPEED = 1000
+
+var motion = Vector2.ZERO
+var isJumping = false 	 	# bool flag to smooth jumping right after moving left/right
 
 func _physics_process(delta):
-	apply_gravity()
-	jump()
-	move()
+	apply_Gravity()
+	Jump()
+	Move()
 	move_and_slide(motion, UP)
 
 
-func apply_gravity():
+func apply_Gravity():
 	if is_on_floor():
 		motion.y = 0
+		isJumping = false
 	else: 
 		motion.y += GRAVITY
 
 
-func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+func Jump():
+	if Input.is_action_just_pressed("jump") and isJumping == false:
+		isJumping = true
 		motion.y -= JUMP_SPEED
 
 
-func move():
+func Move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
